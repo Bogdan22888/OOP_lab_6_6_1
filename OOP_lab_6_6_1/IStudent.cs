b021079a56b05f142a03ba8771818e02d26e963a
Роман Данилевич <dwordchanel@gmail.com>
2020-06-06 20:25:27 +0300

    Add project files.

---
+++ OOP_lab_6_6_1/IStudent.cs
@@ -0,0 +1,16 @@
+﻿namespace OOP_lab_6_6_1
+{
+	interface IStudent
+	{
+		string GetLastName();
+		int GetGradebookId();
+		int GetRating();
+		void SetLastName(string surname);
+		void SetGradebookId(int id);
+		void SetRating(int r);
+		void Sort(Student[] students);
+		double GetAverageRating(Student[] students);
+		int GetUnderAverageAmount(Student[] students);
+		void Output(Student[] students);
+	}
+}
